89e054be3a79752ad49ef14a7bde2c079bcfe2b8
AlexNoVibe <98186596+AlexNoVibe@users.noreply.github.com>
2026-09-21 12:07:28 +0300

    request admin right

--- restart_docker.ps1
+++ restart_docker.ps1
@@ -1,3 +1,9 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "requesting administrator rights..."
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
 Get-Process *docker* | Stop-Process -Force
 Get-Process *wsl* | Stop-Process -Force
 Restart-Service com.docker.service
